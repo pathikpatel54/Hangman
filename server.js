58a1c8013cb37d9fa419b6794b8826fa21aed38b
Pathik Patel <pathikpatel54@gmail.com>
2023-04-28 18:16:23 -0400

    make it better

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 
 app.use("/api", routes);
 
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "tasks-client", "build", "index.html"));
+});
+
 app.listen(keys.PORT, () => {
   console.log(`Server started on port ${keys.PORT}...`);
 });
