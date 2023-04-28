d83f6354914b2c3d6f2de2878ad52d057d58bfde
Pathik Patel <pathikpatel54@gmail.com>
2023-04-28 18:18:30 -0400

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use("/api", routes);
 app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "tasks-client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(keys.PORT, () => {
